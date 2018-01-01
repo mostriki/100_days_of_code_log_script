9320feafcbd5cdb34384b99370dfad43c50201c5
Riki Montgomery <mostriki820@gmail.com>
2018-01-01 14:35:04 -0800

    Create initial script for automating 100 log entries

--- 100_days_of_code_log_script.rb
+++ 100_days_of_code_log_script.rb
@@ -1,21 +1,25 @@
 #!/usr/bin/env ruby
 require 'fileutils'
+require 'date'
+
 
 Dir.chdir "/Users/Guest/Desktop"
 FileUtils.touch "100_days_of_code_log.md"
 
 
 File.open("100_days_of_code_log.md", 'w') do |file|
-  day = 4
-  file.write(
-"# 100 Days Of Code - Log
+  file.write("# 100 Days Of Code - Log")
+  day = 1
+  date = Date.today
+  until day == 101
+  file.write("### Day #{day}: #{date}
 
-### Day #{day}: February 30, 2016 (Example 1)
-##### (delete me or comment me out)
+**Today's Progress**:
 
-**Today's Progress**: Fixed CSS, worked on canvas functionality for the app.
+**Thoughts:**
 
-**Thoughts:** I really struggled with CSS, but, overall, I feel like I am slowly getting better at it. Canvas is still new for me, but I managed to figure out some basic functionality.
-
-**Link to work:** [Calculator App](http://www.example.com)")
+**Link to work:** [Example App](http://www.example.com)")
+  day += 1
+  date += 1
+  end
 end
