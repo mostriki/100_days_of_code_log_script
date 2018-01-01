42cfe442bdcce4e00fc7e734e07c52104f06adfc
Riki Montgomery <mostriki820@gmail.com>
2018-01-01 14:43:42 -0800

    Update markdown entries.

--- 100_days_of_code_log_script.rb
+++ 100_days_of_code_log_script.rb
@@ -8,18 +8,18 @@ FileUtils.touch "100_days_of_code_log.md"
 
 
 File.open("100_days_of_code_log.md", 'w') do |file|
-  file.write("# 100 Days Of Code - Log")
-  day = 1
+  number = 1
   date = Date.today
   until day == 101
-  file.write("### Day #{day}: #{date}
+  file.write(
+"### Log #{number}: #{date}
 
 **Today's Progress**:
 
-**Thoughts:**
+**Thoughts**:
 
 **Link to work:** [Example App](http://www.example.com)")
-  day += 1
+  number += 1
   date += 1
   end
 end
